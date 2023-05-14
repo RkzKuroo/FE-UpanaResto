a48c672ed67f86c8511ee79140abd05a36ad2587
RkzKuroo <ilhamizdam16@gmail.com>
2023-05-14 11:57:13 +0800

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { BiBowlHot } from "react-icons/bi";
+import { MdLocalDrink } from "react-icons/md";
+import { GiHamburger } from "react-icons/gi";
+import { FaIceCream } from "react-icons/fa";
 const Home = () => {
     return(
         <div className="container-home">
             <h1>Menu Terlaris Bulan Ini</h1>
         <div className="menu-1">
-            <object> <BiBowlHot/> </object>
+             <BiBowlHot/> 
         </div>
-        <div className="menu-2"></div>
-        <div className="menu-3"></div>
-        <div className="menu-4"></div>
+        <div className="menu-2">
+             <MdLocalDrink/>
+        </div>
+        <div className="menu-3">
+            <GiHamburger/>
+        </div>
+        <div className="menu-4">
+            <FaIceCream/>
+        </div>
         <div className="pict-1"></div>
         <div className="pict-2"></div>
         <div className="ttl-plng"></div>
